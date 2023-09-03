9d84a7d0ba89887f064e9d2798077c874602d425
Antonin Catrix <bpis@hotmail.fr>
2023-09-03 17:47:55 +0200

    Feat: useMonsters hook added to retrieve monsters from API

---
+++ src/hooks/useMonsters.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { Monster } from '../types';
+
+interface IUseMonsters {
+  data: Monster[],
+  loading: boolean,
+  error: any
+}
+
+const useMonsters = (url: string): IUseMonsters => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+
+        if(response.ok) {
+          const json = await response.json();
+
+          setData(json.results);
+          setLoading(false);
+        } else {
+          throw new Error(`Error with status ${response.status}`)
+        }
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useMonsters;
---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type Monster = {
+  name: string,
+  skills: Skill,
+  actions: Action[]
+}
+
+type Skill = {
+  history: number,
+  perception: number
+}
+
+type Action = {
+  name: string
+}
